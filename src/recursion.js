88568e1f02163e28584bbec8bce4d52fb6495441
Kingsley Allen <kingsley.allen@gmail.com>
2021-01-11 19:28:21 -0600

    Finished reverse

--- src/recursion.js
+++ src/recursion.js
@@ -190,10 +190,27 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+
+  //base cases
+  if (string.length === 0) {
+    return '';
+  }
+
+  if (string.length === 1) {
+    return string;
+  }
+
+  var firstChar = string[0];
+  var lastChar = string[string.length - 1];
+  var coreChars = string.substring(1, string.length - 1);
+  return lastChar + reverse(coreChars) + firstChar;
+
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
